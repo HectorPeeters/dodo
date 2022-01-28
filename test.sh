ac7d5da25ff1da422c13d0314348d87527c93b19
Hector Peeters <hector.peeters@gmail.com>
2022-01-28 16:22:38 +0100

    Make the test script compile the generated assembly

--- test.sh
+++ test.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 cargo b
-for i in tests/*.dodo; do echo "Running" $i; target/debug/dodo $i; done
+for i in tests/*.dodo; do echo "Running" $i; target/debug/dodo $i && nasm -f elf64 output.asm -o /tmp/output.o -g; done
