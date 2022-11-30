3a42fb6a18f67be80a0f5f3ee35707c3006ae91e
Hector Peeters <hector.peeters@gmail.com>
2022-11-30 22:41:21 +0100

    Add tests for optimizations

--- tests/lib.rs
+++ tests/lib.rs
@@ -15,19 +15,46 @@ use std::{
     hash::{Hash, Hasher},
     path::{Path, PathBuf},
 };
+use test_generator::test_resources;
 
-#[allow(dead_code)]
-pub fn run_passing_test(file: &str, backend_type: BackendType) -> Result<()> {
+#[test_resources("tests/data/*.dodo")]
+fn test_c(path: &str) {
+    run_passing_test(path, BackendType::C, false);
+}
+
+#[test_resources("tests/data/*.dodo")]
+fn test_c_optimized(path: &str) {
+    run_passing_test(path, BackendType::C, true);
+}
+
+#[test_resources("tests/data/*.dodo")]
+fn test_x86(path: &str) {
+    run_passing_test(path, BackendType::X86, false);
+}
+
+#[test_resources("tests/data/*.dodo")]
+fn test_x86_optimized(path: &str) {
+    run_passing_test(path, BackendType::X86, true);
+}
+
+#[test_resources("tests/failing/*.dodo")]
+fn test_c_failing(path: &str) {
+    run_failing_test(path, BackendType::C);
+}
+
+#[test_resources("tests/failing/*.dodo")]
+fn test_x86_failing(path: &str) {
+    run_failing_test(path, BackendType::X86);
+}
+
+pub fn run_passing_test(file: &str, backend_type: BackendType, enable_optimization: bool) {
     let source = std::fs::read_to_string(file).unwrap();
     let expected = std::fs::read_to_string(PathBuf::from(file).with_extension("txt")).unwrap();
 
-    let output = run_test(file, &source, backend_type)?;
+    let output = run_test(file, &source, backend_type, enable_optimization).unwrap();
     assert_eq!(output.trim(), expected.trim());
-
-    Ok(())
 }
 
-#[allow(dead_code)]
 pub fn run_failing_test(file: &str, backend_type: BackendType) {
     let source = std::fs::read_to_string(file).unwrap();
 
@@ -36,7 +63,7 @@ pub fn run_failing_test(file: &str, backend_type: BackendType) {
         .find(|x| x.starts_with("// Expected: "))
         .map(|x| x.replace("// Expected: ", ""));
 
-    let result = run_test(file, &source, backend_type);
+    let result = run_test(file, &source, backend_type, false);
     assert!(result.is_err());
 
     if let Some(expected) = expected {
@@ -44,11 +71,17 @@ pub fn run_failing_test(file: &str, backend_type: BackendType) {
     }
 }
 
-fn run_test(file: &str, source: &str, backend_type: BackendType) -> Result<String> {
+fn run_test(
+    file: &str,
+    source: &str,
+    backend_type: BackendType,
+    enable_optimization: bool,
+) -> Result<String> {
     println!("RUNNING '{}' with backend '{:?}'...", file, backend_type);
 
     let mut hasher = DefaultHasher::new();
     file.hash(&mut hasher);
+    enable_optimization.hash(&mut hasher);
     let test_code = hasher.finish();
 
     let tokens = tokenize(source)?;
@@ -64,7 +97,9 @@ fn run_test(file: &str, source: &str, backend_type: BackendType) -> Result<Strin
         .map(|x| type_checker.check_upper_statement(x))
         .collect::<Result<Vec<_>>>()?;
 
-    statements = optimize(statements);
+    if enable_optimization {
+        statements = optimize(statements);
+    }
 
     let mut backend: Box<dyn Backend> = match backend_type {
         BackendType::C => Box::new(CBackend::new(&mut project)),
