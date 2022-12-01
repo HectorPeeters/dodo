7890e602a5e144882cebdb89eabe97ad5b0f5a72
Hector Peeters <hector.peeters@gmail.com>
2022-12-01 14:48:07 +0100

    Add mathematical identity optimisations

--- src/optimisations/math_identities.rs
+++ src/optimisations/math_identities.rs
@@ -1,10 +1,16 @@
-use crate::ast::{BinaryOperatorExpr, BinaryOperatorType, Expression};
+use crate::ast::{BinaryOperatorExpr, BinaryOperatorType, Expression, IntegerLiteralExpr};
 
 use super::ast_walker::AstWalker;
 use super::OptimisationStep;
 
 // This optimisation performs the following transformations
 // x + 0 -> x
+// x - 0 -> x
+// 0 + x -> x
+// x * 0 -> 0
+// 0 * x -> 0
+// x * 1 -> x
+// 1 * x -> x
 
 #[derive(Default)]
 pub struct MathIdentities {
@@ -25,15 +31,51 @@ impl<'a> AstWalker<'a> for MathIdentities {
     fn visit_binop(&mut self, binop: BinaryOperatorExpr<'a>) -> Expression<'a> {
         match (binop.op_type, binop.left, binop.right) {
             (
-                BinaryOperatorType::Add,
-                box Expression::IntegerLiteral(left),
+                BinaryOperatorType::Add | BinaryOperatorType::Subtract,
+                left,
                 box Expression::IntegerLiteral(right),
-            ) if left.value == 0 => Expression::IntegerLiteral(right),
-            (
-                BinaryOperatorType::Add,
-                box Expression::IntegerLiteral(left),
-                box Expression::IntegerLiteral(right),
-            ) if right.value == 0 => Expression::IntegerLiteral(left),
+            ) if right.value == 0 => {
+                self.performed_optimisations += 1;
+                *left
+            }
+            (BinaryOperatorType::Add, box Expression::IntegerLiteral(left), right)
+                if left.value == 0 =>
+            {
+                self.performed_optimisations += 1;
+                *right
+            }
+            (BinaryOperatorType::Multiply, _, box Expression::IntegerLiteral(right))
+                if right.value == 0 =>
+            {
+                self.performed_optimisations += 1;
+                Expression::IntegerLiteral(IntegerLiteralExpr {
+                    value: 0,
+                    type_id: binop.type_id,
+                    range: binop.range,
+                })
+            }
+            (BinaryOperatorType::Multiply, box Expression::IntegerLiteral(left), _)
+                if left.value == 0 =>
+            {
+                self.performed_optimisations += 1;
+                Expression::IntegerLiteral(IntegerLiteralExpr {
+                    value: 0,
+                    type_id: binop.type_id,
+                    range: binop.range,
+                })
+            }
+            (BinaryOperatorType::Multiply, left, box Expression::IntegerLiteral(right))
+                if right.value == 1 =>
+            {
+                self.performed_optimisations += 1;
+                *left
+            }
+            (BinaryOperatorType::Multiply, box Expression::IntegerLiteral(left), right)
+                if left.value == 1 =>
+            {
+                self.performed_optimisations += 1;
+                *right
+            }
             x => Expression::BinaryOperator(BinaryOperatorExpr {
                 op_type: x.0,
                 left: x.1,
