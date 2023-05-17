48633f51fa49b6e7e454c0ca4925dc03927ca085
Hector Peeters <hector.peeters@gmail.com>
2023-05-17 19:29:21 +0200

    Add inkwell context

--- src/backends/llvm_backend.rs
+++ src/backends/llvm_backend.rs
@@ -1,16 +1,29 @@
 use std::path::Path;
 
+use inkwell::{builder::Builder, context::Context, module::Module};
+
 use crate::{ast::UpperStatement, project::Project};
 
 use super::Backend;
 
-pub struct LlvmBackend<'a> {
+pub struct LlvmBackend<'a, 'ctx> {
     project: &'a mut Project,
+    context: Context,
+    builder: Builder<'ctx>,
+    module: Module<'ctx>,
 }
 
 impl<'a> LlvmBackend<'a> {
     pub fn new(project: &'a mut Project) -> Self {
-        Self { project }
+        let context = Context::create();
+        let builder = context.create_builder();
+        let module = context.create_module("main_module");
+        Self {
+            project,
+            context,
+            builder,
+            module,
+        }
     }
 }
 
